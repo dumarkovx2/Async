ad0ed73d28ab0dd6b51c2d057c647dfaa4fc4125
David Markovitz <dumarkov@microsoft.com>
2022-10-02 14:23:48 +0300

    added await MyClass.Linger(4)

--- Program.cs
+++ Program.cs
@@ -6,22 +6,25 @@ public static class MyClass
 {
 	public static async Task Main()
 	{
-        var t3 = MyClass.Linger(3);
-        var t2 = MyClass.Linger(2);
-        var t1 = MyClass.Linger(1);
+        await MyClass.Linger(4); // sync. Might catch 2 threads. anti-pattern.
 
-        await t1;
+        var t3 = MyClass.Linger(3); // async
+        var t2 = MyClass.Linger(2); // async
+        var t1 = MyClass.Linger(1); // async
+
+        await t1; // wait for completion
         Console.WriteLine($"{DateTime.Now} 1 completed");
-        await t3;
+        await t3; // wait for completion
         Console.WriteLine($"{DateTime.Now} 3 completed");
-        await t2;
+        await t2; // wait for completion
         Console.WriteLine($"{DateTime.Now} 2 completed");
 	}
 
     public static async Task<int> Linger(int delay_sec)
     {
         Console.WriteLine($"{DateTime.Now} {delay_sec} Start");
-        await Task.Delay(delay_sec * 1000);
+        var t = Task.Delay(delay_sec * 1000);
+        await t;
         Console.WriteLine($"{DateTime.Now} {delay_sec} End");
         return delay_sec;
     }    
